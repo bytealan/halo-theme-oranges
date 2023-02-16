0bde347eff37cd6e8811ff0a7d5756a8e56b6753
WuWenL <wuwenl0405@sina.cn>
2023-02-16 21:37:46 +0800

    feat: add catalog and tocbot plugin

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import "./styles/main.styl";
+import * as tocbot from "tocbot";
 
-export function count(x: number, y: number) {
-  return x + y;
+export function generateToc() {
+  tocbot.init({
+    tocSelector: ".catalog-content",
+    contentSelector: "#content",
+    headingSelector: "h1, h2, h3, h4",
+    extraListClasses: "toc",
+    // extraLinkClasses:"",
+    activeLinkClass: "active",
+    collapseDepth: 6,
+    headingsOffset: 100,
+    scrollSmooth: true,
+    scrollSmoothOffset: -100,
+  });
 }
